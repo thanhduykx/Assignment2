5f7803cbc15996f27142b4bb4c17814b7751135c
Dương Thành Thanh Duy <60205284+thanhduykx@users.noreply.github.com>
2026-06-13 03:53:23 +0700

    a
    a

--- DataAccessLayer/Enums/ExperimentStatus.cs
+++
@@ -1,9 +0,0 @@
-namespace DataAccessLayer.Enums;
-
-public enum ExperimentStatus
-{
-    Draft = 1,
-    Running = 2,
-    Completed = 3,
-    Failed = 4
-}
--- DataAccessLayer/Enums/UserRole.cs
+++
@@ -1,8 +0,0 @@
-namespace DataAccessLayer.Enums;
-
-public enum UserRole
-{
-    Student = 1,
-    Teacher = 2,
-    Admin = 3
-}
